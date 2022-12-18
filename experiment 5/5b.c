9916f1fbd70b8542a36808c6f78c2148fdb155db
dhruvg1234 <106734968+dhruvg1234@users.noreply.github.com>
2022-12-19 02:47:24 +0530

    Add files via upload

---
+++ experiment 5/5b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+char str[1000];
+int i;
+
+printf("Enter the string: ");
+scanf("%s", str);
+
+for (i = 0; str[i] != '\0'; ++i);
+
+printf("Length of string is %d", i);
+return 0;
+}
